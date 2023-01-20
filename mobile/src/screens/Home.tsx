0c703ffc5cdb0131f054b768ca957e341abd2540
UbiraEFC <ubirata.efc@gmail.com>
2023-01-19 23:58:56 -0300

    feat: add useNavigation to Home

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -9,6 +10,9 @@ const minimumSummaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearsStart.length;
 
 export function Home() {
+
+	const { navigate } = useNavigation();
+
 	return (
 		<View className="flex-1 bg-background px-8 pt-16">
 			<Header />
@@ -35,6 +39,7 @@ export function Home() {
 						datesFromYearsStart.map(date => (
 							<HabitDay
 								key={date.toISOString()}
+								onPress={() => navigate('habit', { date: date.toISOString() })}
 							/>
 						))
 					}
@@ -43,7 +48,7 @@ export function Home() {
 							.from({ length: amountOfDaysToFill })
 							.map((_, i) => (
 								<View
-									key = {i}
+									key={i}
 									className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
 									style={{ width: DAY_SIZE, height: DAY_SIZE }}
 								/>
